9b7911a1aa1f5005cecf762881eb72deebfc99de
Thinkadham <105986824+Thinkadham@users.noreply.github.com>
2025-04-24 02:58:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+class Config:
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+    FONT_PATH = os.path.join(os.path.dirname(__file__), "fonts", "DejaVuSans.ttf")

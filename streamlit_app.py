129d9f435ff419af30c93b50295c901a7d2a0fe0
Thinkadham <105986824+Thinkadham@users.noreply.github.com>
2025-04-23 23:38:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -214,7 +214,7 @@ def create_new_bill():
                 "total_deduction": total_deduction,
                 "net_amount": net_amount,
                 "amount_in_words": f"Rupees {net_amount:,}"  # Just shows the numeric value
-                "cc_bill": cc_bill,
+               
                 "final_bill": final_bill == "Yes",
                 "allotment_no": allotment_no,
                 "allotment_date": allotment_date.isoformat(),

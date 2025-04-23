6d29f2f8df1ef0ee9fec246b8b38521515a004fa
Thinkadham <105986824+Thinkadham@users.noreply.github.com>
2025-04-24 03:04:15 +0530

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,1 +1,15 @@
+from .dashboard import show as show_dashboard
+from .create_bill import show as show_create_bill
+from .contractors import show as show_contractors
+from .works import show as show_works
+from .reports import show as show_reports
+from .settings import show as show_settings
 
+__all__ = [
+    'show_dashboard',
+    'show_create_bill',
+    'show_contractors',
+    'show_works',
+    'show_reports',
+    'show_settings'
+]

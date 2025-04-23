815efc54ed621a247b83734fa5c07c0a00798fbd
Thinkadham <105986824+Thinkadham@users.noreply.github.com>
2025-04-24 03:08:54 +0530

    Update __init__.py

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,9 +1,9 @@
-from .dashboard import show as show_dashboard
-from .create_bill import show as show_create_bill
-from .contractors import show as show_contractors
-from .works import show as show_works
-from .reports import show as show_reports
-from .settings import show as show_settings
+from hydraulicuri.pages.dashboard import show as show_dashboard
+from hydraulicuri.pages.create_bill import show as show_create_bill
+from hydraulicuri.pages.contractors import show as show_contractors
+from hydraulicuri.pages.works import show as show_works
+from hydraulicuri.pages.reports import show as show_reports
+from hydraulicuri.pages.settings import show as show_settings
 
 __all__ = [
     'show_dashboard',

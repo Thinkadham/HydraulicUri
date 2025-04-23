d253ba7cea0bd922940ba52bb83856d422a8c144
Thinkadham <105986824+Thinkadham@users.noreply.github.com>
2025-04-24 03:37:19 +0530

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,7 +1,7 @@
-from hydraulicuri.services.contractor_service import get_contractors, add_contractor
-from hydraulicuri.services.work_service import get_works, add_work
-from hydraulicuri.services.bill_service import create_bill, get_recent_bills
-from hydraulicuri.services.pdf_service import generate_payorder_pdf
+from .contractor_service import get_contractors, add_contractor
+from .work_service import get_works, add_work
+from .bill_service import create_bill, get_recent_bills
+from .pdf_service import generate_payorder_pdf
 
 __all__ = [
     'get_contractors', 

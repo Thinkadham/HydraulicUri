03fdf0118e4335bb9906def74c6a34bf39d69dc3
Thinkadham <105986824+Thinkadham@users.noreply.github.com>
2025-04-24 15:54:00 +0530

    Delete pages/__init__.py

--- pages/__init__.py
+++
@@ -1,6 +0,0 @@
-from .dashboard import show as show_dashboard
-from .create_bill import show as show_create_bill
-from .contractors import show as show_contractors
-from .works import show as show_works
-from .reports import show as show_reports
-from .settings import show as show_settings

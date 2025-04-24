a50f8a99a1a380d9b1c50f263a8ee273017c6b70
Thinkadham <105986824+Thinkadham@users.noreply.github.com>
2025-04-24 14:53:33 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hydraulicuri",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "streamlit",
+        "supabase",
+        "fpdf2",
+        "num2words",
+        "python-dotenv"
+    ],
+)

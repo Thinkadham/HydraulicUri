d9a1481d971a0ab08b273eeda2596f5c22e561bc
Thinkadham <105986824+Thinkadham@users.noreply.github.com>
2025-04-24 15:51:57 +0530

    Delete services directory

--- services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .contractor_service import get_contractors, add_contractor
-from .work_service import get_works, add_work
-from .bill_service import create_bill, get_recent_bills
-from .pdf_service import generate_payorder_pdf

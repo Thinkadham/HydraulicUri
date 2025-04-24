c03299880acec7032a1a176e8ff09ef3c818fcb2
Thinkadham <105986824+Thinkadham@users.noreply.github.com>
2025-04-25 04:53:38 +0530

    Update form_manager.py

--- utils/form_manager.py
+++ utils/form_manager.py
@@ -1,3 +1,4 @@
+# utils/form_manager.py
 import streamlit as st
 import hashlib
 
@@ -12,47 +13,42 @@ class FormManager:
         base_key = f"{self.page_name}_{form_name}_{self.form_counter}"
         return hashlib.md5(base_key.encode()).hexdigest()[:10]
     
+    def form(self, form_name, **kwargs):
+        """Create form context"""
+        form_key = self.get_form_key(form_name)
+        return st.form(key=form_key, **kwargs)
+    
+    def form_submit_button(self, form_name, label="Submit", **kwargs):
+        """Create form submit button"""
+        form_key = self.get_form_key(form_name)
+        return st.form_submit_button(label, key=f"{form_key}_submit", **kwargs)
+    
     def text_input(self, label, form_name, field_name, **kwargs):
-        """Create text input with automatic key"""
+        """Create text input"""
         form_key = self.get_form_key(form_name)
         return st.text_input(label, key=f"{form_key}_{field_name}", **kwargs)
     
     def number_input(self, label, form_name, field_name, **kwargs):
-        """Create number input with automatic key"""
+        """Create number input"""
         form_key = self.get_form_key(form_name)
         return st.number_input(label, key=f"{form_key}_{field_name}", **kwargs)
     
     def selectbox(self, label, form_name, field_name, options, **kwargs):
-        """Create selectbox with automatic key"""
+        """Create selectbox"""
         form_key = self.get_form_key(form_name)
         return st.selectbox(label, options, key=f"{form_key}_{field_name}", **kwargs)
     
-    def form(self, form_name, **kwargs):
-        """Create form with automatic key"""
+    def text_area(self, label, form_name, field_name, **kwargs):
+        """Create text area"""
         form_key = self.get_form_key(form_name)
-        return st.form(key=form_key, **kwargs)
-
-def text_area(self, label, form_name, field_name, **kwargs):
-    """Create text area with automatic key"""
-    form_key = self.get_form_key(form_name)
-    return st.text_area(label, key=f"{form_key}_{field_name}", **kwargs)
-
-def radio(self, label, form_name, field_name, options, **kwargs):
-    """Create radio buttons with automatic key"""
-    form_key = self.get_form_key(form_name)
-    return st.radio(label, options, key=f"{form_key}_{field_name}", **kwargs)
-
-def date_input(self, label, form_name, field_name, **kwargs):
-    """Create date input with automatic key"""
-    form_key = self.get_form_key(form_name)
-    return st.date_input(label, key=f"{form_key}_{field_name}", **kwargs)
-
-def form_submit_button(self, form_name, label="Submit", **kwargs):
-    """Create form submit button with automatic key"""
-    form_key = self.get_form_key(form_name)
-    return st.form_submit_button(label, key=f"{form_key}_submit", **kwargs)
-
-def file_uploader(self, label, form_name, field_name, **kwargs):
-    """Create file uploader with automatic key"""
-    form_key = self.get_form_key(form_name)
-    return st.file_uploader(label, key=f"{form_key}_{field_name}", **kwargs)
+        return st.text_area(label, key=f"{form_key}_{field_name}", **kwargs)
+    
+    def date_input(self, label, form_name, field_name, **kwargs):
+        """Create date input"""
+        form_key = self.get_form_key(form_name)
+        return st.date_input(label, key=f"{form_key}_{field_name}", **kwargs)
+    
+    def radio(self, label, form_name, field_name, options, **kwargs):
+        """Create radio buttons"""
+        form_key = self.get_form_key(form_name)
+        return st.radio(label, options, key=f"{form_key}_{field_name}", **kwargs)

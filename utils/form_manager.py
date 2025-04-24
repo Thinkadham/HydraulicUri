f739893c71c99068371098ad75fa0b133935f231
Thinkadham <105986824+Thinkadham@users.noreply.github.com>
2025-04-25 03:59:32 +0530

    Update form_manager.py

--- utils/form_manager.py
+++ utils/form_manager.py
@@ -31,3 +31,23 @@ class FormManager:
         """Create form with automatic key"""
         form_key = self.get_form_key(form_name)
         return st.form(key=form_key, **kwargs)
+
+def text_area(self, label, form_name, field_name, **kwargs):
+    """Create text area with automatic key"""
+    form_key = self.get_form_key(form_name)
+    return st.text_area(label, key=f"{form_key}_{field_name}", **kwargs)
+
+def radio(self, label, form_name, field_name, options, **kwargs):
+    """Create radio buttons with automatic key"""
+    form_key = self.get_form_key(form_name)
+    return st.radio(label, options, key=f"{form_key}_{field_name}", **kwargs)
+
+def date_input(self, label, form_name, field_name, **kwargs):
+    """Create date input with automatic key"""
+    form_key = self.get_form_key(form_name)
+    return st.date_input(label, key=f"{form_key}_{field_name}", **kwargs)
+
+def form_submit_button(self, form_name, label="Submit", **kwargs):
+    """Create form submit button with automatic key"""
+    form_key = self.get_form_key(form_name)
+    return st.form_submit_button(label, key=f"{form_key}_submit", **kwargs)

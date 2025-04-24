51b15a919d3b6f34f0eb8b3c570b158a547e0f4a
Thinkadham <105986824+Thinkadham@users.noreply.github.com>
2025-04-25 03:29:21 +0530

    Create form_manager.py

---
+++ utils/form_manager.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import hashlib
+
+class FormManager:
+    def __init__(self, page_name):
+        self.page_name = page_name
+        self.form_counter = 0
+    
+    def get_form_key(self, form_name):
+        """Generate unique form key"""
+        self.form_counter += 1
+        base_key = f"{self.page_name}_{form_name}_{self.form_counter}"
+        return hashlib.md5(base_key.encode()).hexdigest()[:10]
+    
+    def text_input(self, label, form_name, field_name, **kwargs):
+        """Create text input with automatic key"""
+        form_key = self.get_form_key(form_name)
+        return st.text_input(label, key=f"{form_key}_{field_name}", **kwargs)
+    
+    def number_input(self, label, form_name, field_name, **kwargs):
+        """Create number input with automatic key"""
+        form_key = self.get_form_key(form_name)
+        return st.number_input(label, key=f"{form_key}_{field_name}", **kwargs)
+    
+    def selectbox(self, label, form_name, field_name, options, **kwargs):
+        """Create selectbox with automatic key"""
+        form_key = self.get_form_key(form_name)
+        return st.selectbox(label, options, key=f"{form_key}_{field_name}", **kwargs)
+    
+    def form(self, form_name, **kwargs):
+        """Create form with automatic key"""
+        form_key = self.get_form_key(form_name)
+        return st.form(key=form_key, **kwargs)
